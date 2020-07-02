92ee270b19e9093e6aa7581fef926766e1c992a8
ReynardVN8 <reynardvnata@yahoo.co.id>
2020-07-02 20:58:16 +0700

    Finally fixed context force close due to need reference of inflater to fragments

--- app/src/main/java/com/isyaratproject/signdictionary/View/DictionaryFragment.kt
+++ app/src/main/java/com/isyaratproject/signdictionary/View/DictionaryFragment.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.isyaratproject.signdictionary.Adapter.RecyclerView.DictionaryAdapter
 import com.isyaratproject.signdictionary.R
+import com.isyaratproject.signdictionary.Utilities.*
 import com.isyaratproject.signdictionary.ViewModel.DictionaryViewModel
 
 class DictionaryFragment : Fragment() {
 
     private lateinit var homeViewModel: DictionaryViewModel
+    lateinit var  categoryAdapter: ArrayAdapter<String>
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -26,8 +32,13 @@ class DictionaryFragment : Fragment() {
                 ViewModelProviders.of(this).get(DictionaryViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dictionary, container, false)
         val categoryTitle: TextView = root.findViewById(R.id.dictionaryCategoryTxt)
-        categoryTitle.text = "Category:"
+        val dictionaryRecyclerVew: RecyclerView = root.findViewById(R.id.dictionaryRecyclerView)
         val categorySpinner: Spinner = root.findViewById(R.id.dictionaryCategorySpinner)
+
+        categoryTitle.text = "Category:"
+        dictionaryRecyclerVew.adapter = DictionaryAdapter(root.context, DictionaryDataService.Dictionary_Abjad_Bisindo)
+        dictionaryRecyclerVew.layoutManager = LinearLayoutManager(root.context)
+        dictionaryRecyclerVew.setHasFixedSize(true)
 
         return root
     }
