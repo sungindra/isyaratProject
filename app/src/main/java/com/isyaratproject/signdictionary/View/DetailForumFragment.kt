7f50c07adf1b87c749496ed2acf904a466c2508d
ReynardVN8 <reynardvnata@yahoo.co.id>
2020-07-28 13:56:21 +0700

    Creating a Layout for upload a answer comment and base functions for it

--- app/src/main/java/com/isyaratproject/signdictionary/View/DetailForumFragment.kt
+++ app/src/main/java/com/isyaratproject/signdictionary/View/DetailForumFragment.kt
@@ -1,12 +1,16 @@
 package com.isyaratproject.signdictionary.View
 
-import androidx.lifecycle.ViewModelProviders
+import android.app.Activity.*
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.isyaratproject.signdictionary.Adapter.DetailForumAdapter
@@ -14,7 +18,11 @@ import com.isyaratproject.signdictionary.R
 import com.isyaratproject.signdictionary.Utilities.CommentDataService
 import com.isyaratproject.signdictionary.ViewModel.DetailForumViewModel
 
+
 class DetailForumFragment : Fragment() {
+
+    private val PICK_IMAGE = 100
+    var imageUri: Uri? = null
 
     companion object {
         fun newInstance() =
@@ -38,13 +46,30 @@ class DetailForumFragment : Fragment() {
         detailRecyclerView.layoutManager = LinearLayoutManager(root.context)
         detailRecyclerView.setHasFixedSize(true)
 
+        
+
         return root
+    }
+
+    fun openGallery(){
+        val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        startActivityForResult(gallery, PICK_IMAGE)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(DetailForumViewModel::class.java)
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            if (data != null) {
+                imageUri = data.data
+            };
+            // imageView.setImageURI(imageUri);
+        }
     }
 
 }
